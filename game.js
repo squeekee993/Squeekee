cf3dd02d59f2056f53c405d5f46e4861b06d7084
squeekee993 <66395168+squeekee993@users.noreply.github.com>
2020-06-04 09:25:32 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-
+var c = document.getElementById("myCanvas")
+var ctx = c.getContext("2d")
+//image
+var images
+createImage("Squeekee-Duel-Menu-Background.png")
+function createImage(incompleteScr){
+	images.push(new Image())
+	images[images.length].scr = "https://i.ibb.co/G3GWSqp/" + incompleteScr
+}
+function requirements(){
+	scrollTo(10, 10);
+	ctx.canvas.width  = window.innerWidth
+	ctx.canvas.height = window.innerHeight
+	pixelWidth = window.innerWidth/1366
+	pixelHeight = window.innerHeight/768
+}
